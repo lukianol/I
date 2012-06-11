36fff2496d79a2f1aefd775cd4bb61fc6c3a1b9a
Oleksandr Lukiantsev <oleksandr.lukiantsev@gmail.com>
2012-06-11 10:43:40 -0400

    first commit of a task number 38

---
+++ Test/Num_38_Test.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using Num._38;
+
+namespace Test
+{
+    [TestFixture]
+    public class Num_38_Test
+    {
+        [Test]
+        public void GetSubArrayWithLargestSum_Finds_That_SubArray()
+        {
+            var ints = new[] {-4, 1, 2, 3, -5};
+            var subArrayWithLargestSum = Main.GetSubArrayWithLargestSum(ints);
+            CollectionAssert.AreEqual(new []{ 1, 2, 3 }, subArrayWithLargestSum);
+        }
+    }
+}
