8782b0850b7cfe75ed8517fd90732608ee106af4
Oleksandr Lukiantsev <oleksandr.lukiantsev@gmail.com>
2012-06-19 00:02:28 +0300

    answer to a question of 57

---
+++ Num.57/Main.cs
@@ -0,0 +1,24 @@
+﻿namespace Num._57
+{
+    public static class Main
+    {
+        public static string ToUppercase(string input)
+        {
+            var charArray = input.ToCharArray();
+
+            for (var i = 0; i < charArray.Length; i++)
+            {
+                var c = charArray[i];
+                if ((c >= 'a') && (c <= 'z'))
+                {
+                    charArray[i] = (char)(c - Offset);
+                }
+
+            }
+
+            return new string(charArray);
+        }
+
+        private const int Offset = 'a' - 'A';
+    }
+}
---
+++ Test/Num_57_Test.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using Num._57;
+
+namespace Test
+{
+    [TestFixture]
+    public class Num_57_Test
+    {
+
+        [ThisTestIsAnswer]
+        [Test]
+        public void Test()
+        {
+            var actual = Main.ToUppercase("Hello world!");
+            Assert.AreEqual("HELLO WORLD!", actual);
+        }
+    }
+}
