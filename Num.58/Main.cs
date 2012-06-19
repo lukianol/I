19cbce80cedb57f3f36acc6b39a3e61b83ececfd
Oleksandr Lukiantsev <oleksandr.lukiantsev@gmail.com>
2012-06-19 09:35:54 -0400

    added an answer for #58

---
+++ Num.58/Main.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Num._58
+{
+    public static class Main
+    {
+        public static ICollection<int> GetUniqueElements(ICollection<int> sortedCollection)
+        {
+            if (sortedCollection == null) 
+                throw new ArgumentNullException("sortedCollection");
+
+            var resultedList = new HashSet<int>();
+
+            int? previous = null;
+
+            foreach (var element in sortedCollection)
+            {
+                if (element == previous) 
+                    continue;
+
+                if (!resultedList.Add(element))
+                    throw new ArgumentException("The list may not be sorted", "sortedCollection");
+
+                previous = element;
+            }
+
+            return resultedList;
+        }
+    }
+}
---
+++ Test/Num_58_Test.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using NUnit.Framework;
+using Num._58;
+
+namespace Test
+{
+    [TestFixture]
+    public class Num_58_Test
+    {
+
+        [ThisTestIsAnswer]
+        [Test]
+        [TestCase(new []{1, 1, 2, 4, 6, 6})]
+        [TestCase(new[] { 1, 1, 2, 4, 6, 6, 7, 7, 9})]
+        public void GetUniqueElements_Returns_Unqiue_Elements_If_List_Is_Sorted(int[] source)
+        {
+            Debug.Assert(source != null, "source != null");
+
+            var actual = Main.GetUniqueElements(source);
+            CollectionAssert.AreEqual(source.Distinct(), actual);
+        }
+
+        [Test]
+        [TestCase(new[] { 1, 1, 2, 6,  4, 6 })]
+        [TestCase(new[] { 1, 2, 1, 4, 6, 6, 7, 7, 9 })]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetUniqueElements_Fails_If_List_Is_Not_Sorted(int[] source)
+        {
+            Debug.Assert(source != null, "source != null");
+
+            var actual = Main.GetUniqueElements(source);
+            CollectionAssert.AreEqual(source.Distinct(), actual);
+        }
+    }
+}
