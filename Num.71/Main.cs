aa5ed6d654cb0cac2c3f42f3f660daee8c3ed177
Oleksandr Lukiantsev <oleksandr.lukiantsev@gmail.com>
2012-06-25 10:25:19 -0400

    added nswer to #71

---
+++ Num.71/Main.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Num._71
+{
+    public static class Main
+    {
+        public static uint DiscreteLog2(uint value)
+        {
+            if (value == 0)
+                throw new ArgumentException("Result is -Infinity for '0'", "value");
+           
+            uint position = sizeof(uint) * 8 - 1;
+            const uint mask = 0x80000000;
+
+            while ((value & mask) != mask)
+            {
+                value = value << 1;
+                position--;
+            }
+
+            return position;
+        }
+    }
+}
---
+++ Test/Num_71_Test.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NUnit.Framework;
+using Num._71;
+
+namespace Test
+{
+    [TestFixture]
+    public class Num_71_Test
+    {
+        [ThisTestIsAnswer]
+        [Test]
+        [TestCase((uint)1)]
+        [TestCase((uint)2)]
+        [TestCase((uint)27)]
+        [TestCase((uint)49)]
+        [TestCase((uint)160)]
+        [TestCase((uint)1284)]
+        public void DiscreteLog2_Works(uint value)
+        {
+            var actual = Main.DiscreteLog2(value);
+            var expected = Convert.ToUInt32(Math.Floor(Math.Log(value, 2)));
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void DiscreteLog2_Fails_OnZero()
+        {
+            Main.DiscreteLog2(0);
+        }
+    }
+}
