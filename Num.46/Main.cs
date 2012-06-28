9bfeafbc156accda138838540aec448264f86bbe
Oleksandr Lukiantsev <oleksandr.lukiantsev@gmail.com>
2012-06-28 23:46:50 +0300

    commit #46

---
+++ Num.46/Main.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Num._46
+{
+    public static class Main
+    {
+        public static void InsertInSortedList<T>(List<T> list, T item)
+        {
+            list.Sort();//just in case it was not sorted
+            var index = list.BinarySearch(item);
+            if (index < 0)
+            {
+                list.Insert(~index, item);
+            }
+        }
+    }
+}
---
+++ Test/Num_46_Test.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using Num._46;
+
+namespace Test
+{
+    [TestFixture]
+    public class Num_46_Test
+    {
+        [Test]
+        public void InsertInSortedList_Works()
+        {
+            var list = new List<int> {1,2,3,5};
+            Main.InsertInSortedList(list, 4);
+            CollectionAssert.AreEqual(new List<int> {1, 2, 3, 4, 5}, list);
+        }
+    }
+}
