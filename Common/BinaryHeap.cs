cd54a4ddec119990be0ea06ca97b4c7e0e6cbae9
Oleksandr Lukiantsev <oleksandr.lukiantsev@gmail.com>
2012-09-06 00:08:14 +0300

    trying to add a binary heap to the Common library

---
+++ Common/BinaryHeap.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Common
+{
+    public class BinaryHeap<T> where T : IComparable
+    {
+        public void Add(T item)
+        {
+            _storage.Add(item);
+
+            var bubbleIndex = _storage.Count - 1;
+
+            while (bubbleIndex != 0)
+            {
+                var parentIndex = (bubbleIndex - 1) / 2;
+
+                if (_storage[bubbleIndex].CompareTo(_storage[parentIndex]) > 0)
+                {
+                    break;
+                }
+
+                var tmpValue = _storage[parentIndex];
+                _storage[parentIndex] = _storage[bubbleIndex];
+                _storage[bubbleIndex] = tmpValue;
+                bubbleIndex = parentIndex;
+            }
+        }
+
+        private readonly List<T> _storage = new List<T>();
+    }
+}
