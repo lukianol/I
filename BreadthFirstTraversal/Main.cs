72917629d0793aeab55cbcb91307c91105ed0fd1
Oleksandr Lukiantsev <oleksandr.lukiantsev@gmail.com>
2012-09-05 07:47:46 -0400

    added breadth-first traversal

---
+++ BreadthFirstTraversal/Main.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Common;
+
+namespace BreadthFirstTraversal
+{
+    public static class Main
+    {
+        public static void TraverseBinaryTreeBreadthFirst<T>(BinaryTree<T> tree, TextWriter writer)
+        {
+            if (tree == null) 
+                throw new ArgumentNullException("tree");
+            if (writer == null) 
+                throw new ArgumentNullException("writer");
+
+            PrintLevel(writer, tree.Root);
+        }
+
+        private static void PrintLevel<T>(TextWriter writer, params BinaryTreeNode<T>[] nodes)
+        {
+            var list = new List<BinaryTreeNode<T>>(nodes.Length * 2);
+            foreach (var node in nodes)
+            {
+                writer.Write(node.Value);
+                AddNode(list, node.Left);
+                AddNode(list, node.Right);
+            }
+            
+            if (list.Count > 0)
+                PrintLevel(writer, list.ToArray());
+        }
+
+        private static void AddNode<T>(ICollection<BinaryTreeNode<T>> list, BinaryTreeNode<T> node)
+        {
+            if (node != null)
+            {
+                list.Add(node);
+            }
+        }
+    }
+}
---
+++ Test/BreadthFirstTraversal_Test.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using Common;
+using NUnit.Framework;
+
+namespace Test
+{
+    [TestFixture]
+    public class BreadthFirstTraversal_Test
+    {
+        [Test]
+        public void Test()
+        {
+            var binaryTree = new BinaryTree<string>
+                                 {
+                                     Root = new BinaryTreeNode<string>("A")
+                                                {
+                                                    Left = new BinaryTreeNode<string>("B")
+                                                               {
+                                                                   Left = new BinaryTreeNode<string>("D")
+                                                               },
+                                                    Right = new BinaryTreeNode<string>("C")
+                                                                {
+                                                                    Right = new BinaryTreeNode<string>("E")
+                                                                }
+                                                }
+                                 };
+
+            using (var writer = new StringWriter())
+            {
+                BreadthFirstTraversal.Main.TraverseBinaryTreeBreadthFirst(binaryTree, writer);
+                Assert.AreEqual("ABCDE", writer.ToString());
+            }
+            
+        }
+    }
+}
